258bd2e588b68e81d72142ab96f5077f7d245393
Rukshan <arkruka@gmail.com>
2024-07-14 17:29:11 +0200

    refactor: replace os args with flags

--- parser/input.go
+++ parser/input.go
@@ -1,46 +1,87 @@
 package parser
 
 import (
-	"encoding/csv"
+	"flag"
 	"fmt"
+	"odk_mailer/models"
+	"odk_mailer/processes"
 	"os"
+	"time"
 )
 
-func ReadData() ([][]string, string) {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: parser <input data sheet> <message template>")
-		os.Exit(1)
-	}
-
-	inputDataSheet := os.Args[1]
-	messageTemplate := os.Args[2]
-
-	if inputDataSheet == "" {
-		fmt.Println("Input data sheet is required")
-		os.Exit(1)
-	}
-	records := readCSV(inputDataSheet)
-
-	if messageTemplate == "" {
-		fmt.Println("Message template is required")
-		os.Exit(1)
-	}
-
-	return records, messageTemplate
+type Input struct {
+	Command string
+	NewJob  processes.NewJob
 }
 
-func readCSV(filePath string) [][]string {
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening file")
-		os.Exit(1)
+func ParseInput() Input {
+	cmd := flag.String("c", "", "Command")
+	help := flag.String("h", "", "Help")
+	inputDataSheet := flag.String("i", "", "Input data sheet")
+	messageTemplate := flag.String("t", "", "Message template")
+	senderEmail := flag.String("s", "", "Sender email")
+	schedule := flag.String("d", time.Now().String(), "Schedule")
+	init := flag.Bool("init", false, "Initialize database")
+	jobHash := flag.String("j", "", "Job hash")
+
+	flag.Parse()
+
+	if *cmd == "h" || *cmd == "help" || *help != "" {
+		fmt.Println("Usage: ./mailer -c new_job -i <input data sheet> -t <message template> -s <sender email> -d <schedule>")
+		os.Exit(0)
 	}
 
-	readFile := csv.NewReader(file)
-	records, err := readFile.ReadAll()
-	if err != nil {
-		fmt.Println("Error reading file")
-		os.Exit(1)
+	if *init {
+		initDb()
+		fmt.Println("Database initialized")
+		os.Exit(0)
 	}
-	return records
+
+	if *jobHash != "" {
+		return Input{Command: "run_job", NewJob: processes.NewJob{Hash: *jobHash}}
+	}
+
+	if *cmd == "new_job" {
+		if *messageTemplate == "" {
+			fmt.Println("Message template is required")
+			os.Exit(1)
+		}
+
+		if *inputDataSheet == "" {
+			fmt.Println("Input data sheet is required")
+			os.Exit(1)
+		}
+
+		if *senderEmail == "" {
+			fmt.Println("Sender email is required")
+			os.Exit(1)
+		}
+
+		var scheduleTime time.Time
+		if *schedule == "" {
+			scheduleTime = time.Now()
+		} else {
+			st, e := time.Parse(time.RFC3339, *schedule)
+			scheduleTime = st
+			if e != nil {
+				panic(e)
+			}
+		}
+		newJob := processes.NewJob{InputFile: *inputDataSheet, TemplateFile: *messageTemplate, Sender: *senderEmail, Schedule: scheduleTime}
+		return Input{Command: *cmd, NewJob: newJob}
+	}
+
+	if *cmd == "list_jobs" {
+		return Input{Command: *cmd}
+	}
+
+	if *cmd == "list_messages" {
+		return Input{Command: *cmd}
+	}
+
+	return Input{Command: *cmd}
+}
+
+func initDb() {
+	models.Migrate()
 }
