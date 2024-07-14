334d24376d04fe2ec46dbd723e8206b469fbfa52
Rukshan <arkruka@gmail.com>
2024-07-14 17:30:48 +0200

    refactor: parse flag inputs

--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"odk_mailer/output"
 	"odk_mailer/parser"
+	"odk_mailer/processes"
 )
 
 func main() {
-	records, message := parser.ReadData()
-	fmt.Println(records)
-	fmt.Println(message)
+	parseCommand := parser.ParseInput()
+	fmt.Println(parseCommand)
+	if parseCommand.Command == "new_job" {
+		fmt.Println("Creating new job")
+		newJob := processes.NewJob(parseCommand.NewJob)
+		generatedNewJob := processes.GenerateNewJob(newJob)
+		fmt.Println(generatedNewJob)
+	}
+
+	if parseCommand.Command == "list_jobs" {
+		listJobs := processes.GetAllJobs()
+		output.GenerateAllJobTable(listJobs)
+	}
+
+	if parseCommand.Command == "list_messages" {
+		fmt.Println(processes.GetAllMessages())
+	}
+
+	if parseCommand.Command == "run_job" {
+		fmt.Println("Running job", parseCommand.NewJob.Hash)
+		processes.RunJob(parseCommand.NewJob.Hash)
+	}
 }
